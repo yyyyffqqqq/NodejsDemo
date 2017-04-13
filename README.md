f29e53b8ec74c4f98e3092fba3a2a9acbbd1967b
yyyyffqqqq <617438868@qq.com>
2017-04-13 21:58:19 +0800

    搭建项目目录结构

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# NodejsDemo
+# 入门学习demo
+## 1、使用Express， 创建项目目录结构，mvc模式；
--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+//To create a virtual path prefix
+app.use('/static', express.static('public'))
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
